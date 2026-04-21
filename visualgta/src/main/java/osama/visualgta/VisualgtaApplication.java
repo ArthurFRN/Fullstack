08596774bbbd466c1393edbeee95ab06c7bbb518
ArthurFRN <fr.coby@duocuc.cl>
2026-04-21 13:33:29 -0400

    Proyecto fullstack
    Primer repositorio

---
+++ visualgta/src/main/java/osama/visualgta/VisualgtaApplication.java
@@ -0,0 +1,13 @@
+package osama.visualgta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualgtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualgtaApplication.class, args);
+	}
+
+}
