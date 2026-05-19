4dd9ba76cf46a027f95f8713f768d2a5b1a86a58
ArthurFRN <fr.coby@duocuc.cl>
2026-05-19 13:55:18 -0400

    Actualizacion del proyecto

---
+++ partidos.proyectoFullstack/partidos.proyectoFullstack/src/main/java/futbol/partidos/proyectoFullstack/dto/JugadorDto.java
@@ -0,0 +1,14 @@
+package futbol.partidos.proyectoFullstack.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JugadorDto {
+
+    private String Nombre;
+    private String NacionalidadJugador;
+}
